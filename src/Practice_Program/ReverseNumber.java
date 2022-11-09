e887f8b327404bad6d20c0ef535916ea5f26fadf
Admin <Admin@HEXANIKA-LTP-07>
2022-11-09 23:46:34 +0530

    New Assignment Dated 09/11/22

--- src/Practice_Program/ReverseNumber.java
+++ src/Practice_Program/ReverseNumber.java
@@ -3,9 +3,31 @@ package Practice_Program;
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Practice Code");
+		ReverseNumber obj = new ReverseNumber();
+		obj.UsingWhileLoop(args);
+		obj.UsingFor();
+	}
+	
+	public void UsingWhileLoop(String[] args) {
+		
+		int number = 100456, reverse=0 ; 
+		while(number!=0)
+		{
+			int remainder = number%10;
+			reverse = reverse *10+ remainder;
+			number = number/10;
+		}
+		System.out.println("The reverse of given number is: "+ reverse);
+	}
+	
+	public void UsingFor() {
+		int number = 100456, reverse=0 ;
+		for (;number!=0;number=number/10)
+		{
+			int remainder = number%10;
+			reverse= reverse*10+remainder;
+		}
+		System.out.println("The reverse of given number is: "+ reverse);
 	}
 
 }
